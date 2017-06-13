f5eb730541947a035b4ce692f36ac69071607b2d
Shubham Verma <lifemeansmore008@gmail.com>
2017-06-13 18:58:12 +0530

    Added images and edited adapter. Changed Appearance too.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -47,7 +47,7 @@ public class NumbersActivity extends AppCompatActivity {
             rootview.addView(word);
         }
         */
-        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        WordAdapter itemsAdapter = new WordAdapter(this, words,R.color.category_numbers);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,11 +3,13 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,11 +20,12 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-
-    public WordAdapter(Context context,ArrayList<Word> data)
+    private int colorchosen;
+    public WordAdapter(Context context,ArrayList<Word> data,int type)
     {
         //second argument is used when we only need a single textview. here we can put any value.
         super(context,0,data);
+        colorchosen=type;
     }
 
     @NonNull
@@ -34,6 +37,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
         if(listview == null) {
             listview = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
+            LinearLayout ll=(LinearLayout) listview.findViewById(R.id.box);
+            ll.setBackgroundColor(ContextCompat.getColor(getContext(),colorchosen));
         }
         Word currentWord=getItem(position);
 
